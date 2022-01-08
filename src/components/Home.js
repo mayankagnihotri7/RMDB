85609621d23e69caf4a75a7189e03532b198d618
Mayank <mayankagnihotri7@gmail.com>
2022-01-08 13:47:59 +0530

    Added custom hook, refactored home component and added API files

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,12 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import API from "../API";
+import { useHomeFetch } from "../hooks/useHomeFetch";
 import NoImage from "../images/no_image.jpg";
 
 const Home = () => {
-  const [state, setState] = useState();
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState();
+  const { state, loading, error } = useHomeFetch();
+
+  console.log(state);
 
   return <div>Home Page</div>;
 };
