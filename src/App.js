d21df7fe02fd30c8439458032a25c2304e0c7592
Mayank <mayankagnihotri7@gmail.com>
2022-01-10 23:08:37 +0530

    Added routing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from "react";
+import { Route, BrowserRouter, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./components/Home";
+import Movie from "./components/Movie";
+import NotFound from "./components/NotFound";
 import { GlobalStyle } from "./GlobalStyle";
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Home />
-      <GlobalStyle />
-    </div>
-  );
-}
+const App = () => (
+  <BrowserRouter>
+    <Header />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/:movieId" element={<Movie />} />
+      <Route path="/*" element={<NotFound />} />
+    </Routes>
+    <GlobalStyle />
+  </BrowserRouter>
+);
 
 export default App;
--- src/components/Thumb/index.js
+++ src/components/Thumb/index.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Image } from "./Thumb.styles";
 
 const Thumb = ({ img, movieId, clickable }) => (
   <div>
-    <Image src={img} alt="movie-thumb" />
+    {clickable ? (
+      <Link to={`/${movieId}`}>
+        <Image src={img} alt="movie-thumb" />
+      </Link>
+    ) : (
+      <Image src={img} alt="movie-thumb" />
+    )}
   </div>
 );
 
