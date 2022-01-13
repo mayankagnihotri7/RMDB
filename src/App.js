b9b53c1b0ac37fa27573594ac909665d89189fe0
Mayank <mayankagnihotri7@gmail.com>
2022-01-13 21:39:53 +0530

    Setup global context

--- src/App.js
+++ src/App.js
@@ -5,16 +5,19 @@ import Home from "./components/Home";
 import Movie from "./components/Movie";
 import NotFound from "./components/NotFound";
 import { GlobalStyle } from "./GlobalStyle";
+import UserProvider from "./context";
 
 const App = () => (
   <BrowserRouter>
-    <Header />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/:movieId" element={<Movie />} />
-      <Route path="/*" element={<NotFound />} />
-    </Routes>
-    <GlobalStyle />
+    <UserProvider>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:movieId" element={<Movie />} />
+        <Route path="/*" element={<NotFound />} />
+      </Routes>
+      <GlobalStyle />
+    </UserProvider>
   </BrowserRouter>
 );
 
