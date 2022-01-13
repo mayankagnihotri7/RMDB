707d96edd9c1e8372cdbc24163e41b5ae27404dd
Mayank <mayankagnihotri7@gmail.com>
2022-01-13 23:12:42 +0530

    Added login button

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import RMDBLogo from "../../images/react-movie-logo.svg";
 import TMDBLogo from "../../images/tmdb_logo.svg";
 import { Content, LogoImg, TMDBLogoImg, Wrapper } from "./Header.styles";
+import { Context } from "../../context";
 
 const Header = () => {
+  const [user] = useContext(Context);
+
   return (
     <Wrapper>
       <Content>
         <Link to="/">
           <LogoImg src={RMDBLogo} alt="rmdb-log" />
         </Link>
+        {user ? (
+          <span>Logged in as: {user.username}</span>
+        ) : (
+          <Link to="/login">
+            <span>Login</span>
+          </Link>
+        )}
         <TMDBLogoImg src={TMDBLogo} alt="tmdb-logo" />
       </Content>
     </Wrapper>
